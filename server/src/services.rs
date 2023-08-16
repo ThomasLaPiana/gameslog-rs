c1418be7d426ab76beb3a06cc792f89d1e394277
Thomas <ThomasLaPiana@users.noreply.github.com>
2023-08-16 16:55:09 +0800

    feat: add path extraction for delete/get and a health endpoint

--- server/src/services.rs
+++ server/src/services.rs
@@ -1,10 +1,16 @@
 use crate::models::Game;
-use axum::{extract, routing::get, routing::post, Json, Router};
+use axum::{extract, routing::delete, routing::get, routing::post, Json, Router};
 use serde_json::{json, Value};
 
 async fn root() -> Json<Value> {
     Json(json!({
         "data": "Congratulations! You've reached the Gameslog server!"
+    }))
+}
+
+async fn health() -> Json<Value> {
+    Json(json!({
+        "data": "We very healthy!"
     }))
 }
 
@@ -14,9 +20,9 @@ async fn list_games() -> Json<Value> {
     }))
 }
 
-async fn get_game() -> Json<Value> {
+async fn get_game(extract::Path(game_id): extract::Path<String>) -> Json<Value> {
     Json(json!({
-        "data": "Here is a game!"
+        "data": format!("Retrieved: {}!", game_id)
     }))
 }
 
@@ -25,9 +31,9 @@ async fn create_game(extract::Json(payload): extract::Json<Game>) -> Json<Game> 
     Json(payload)
 }
 
-async fn delete_game() -> Json<Value> {
+async fn delete_game(extract::Path(game_id): extract::Path<String>) -> Json<Value> {
     Json(json!({
-        "data": "Deleted a game!"
+        "data": format!("Deleted: {}!", game_id)
     }))
 }
 
@@ -35,8 +41,10 @@ pub fn create_games_router() -> Router {
     let router = Router::new()
         // Add Routes
         .route("/", get(root))
+        .route("/health", get(health))
         .route("/games", get(list_games))
         .route("/games", post(create_game))
-        .route("/games/:id", get(get_game));
+        .route("/games/:game_id", delete(delete_game))
+        .route("/games/:game_id", get(get_game));
     router
 }
